19a826dbfcd1547a276bb1471862c86fe9407061
db <leonardo.taauge@gmail.com>
2022-09-27 18:22:40 -0300

    frist

---
+++ src/test/java/CentroDistribuicaoTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.*;
+public class CentroDistribuicaoTest {
+    private CentroDistribuicaoTest centroDistribuicaoTest = null;
+
+//    @BeforeEach
+//    void setUp() {
+//        gerenciaLugares = new GerenciaLugares();
+//    }
+//
+//    @Test
+//    public void QP1A1O2(){
+//        gerenciaLugares.populaAssentos(30);
+//        int result = gerenciaLugares.verificaLugar("F20A12");
+//        Assertions.assertEquals(3,result);
+//    }
+}
